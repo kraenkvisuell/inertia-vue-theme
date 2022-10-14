7450b9697207f51fac576216bee0d93be6e57bf8
Sebastian Hilger <hilger@kraenk.de>
2022-10-14 17:10:57 +0200

    wip

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,12 +46,17 @@ return [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'endpoint' => env('AWS_ENDPOINT'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            // 'visibility' => 'public', // https://statamic.dev/assets#visibility
+            'visibility' => 'public',
+            'root' => env('APP_SLUG', 'default-website'),
+
+            'cache' => [
+                'store' => 'redis',
+                'expire' => 300,
+                'prefix' => 'assets',
+            ],
         ],
 
         'assets' => [
