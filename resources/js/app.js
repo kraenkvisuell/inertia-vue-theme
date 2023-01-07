e47ef3f3e0be5c8d2523d56224e8dce5845cc1ab
Sebastian Hilger <hilger@kraenk.de>
2023-01-07 18:44:37 +0100

    change pp.js and gitignore

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
-import { createApp, h } from "vue";
-import { createInertiaApp } from "@inertiajs/inertia-vue3";
-import Layout from "@/Layouts/Layout.vue";
-import { InertiaProgress } from "@inertiajs/progress";
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import Layout from '@/Layouts/Layout.vue'
+import { InertiaProgress } from '@inertiajs/progress'
 
 InertiaProgress.init({
     color: '#a2efd6',
@@ -10,14 +10,14 @@ InertiaProgress.init({
 createInertiaApp({
     title: (title) => `${title}`,
     resolve: name => {
-        const page = require(`./Blueprints/${name}`).default;
-        page.layout = page.layout || Layout;
-        return page;
+        const page = require(`./Blueprints/${name}`).default
+        page.layout = page.layout || Layout
+        return page
     },
     
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
-            .mount(el);
+            .mount(el)
     },
 });
